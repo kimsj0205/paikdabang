d0e7c7590058140bf690eceea48ec815a9338de5
kimsj0205 <kkeeyy0205@naver.com>
2023-08-21 12:15:22 +0900

    ver 1.0.1

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@
 setInterval(function(){
 
   $('.fig1 ul').animate({
-    left : '-1920px'
+    left : '-2400px'
   }, 600, function(){
     $('.fig1 ul').append($('.fig1 ul li').eq(0));
     $('.fig1 ul').css({ left : 0 });
   });
 
 },3000);
-
-// popup menu
