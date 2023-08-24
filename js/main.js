43159227f02bc7f2d918f1a9e89408d2227a7d1a
kimsj0205 <kkeeyy0205@naver.com>
2023-08-24 16:43:51 +0900

    "ver 1.0.4
    css file change

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 setInterval(function(){
 
   $('.fig1 ul').animate({
-    left : '-2140px'
+    left : '-1920px'
   }, 600, function(){
     $('.fig1 ul').append($('.fig1 ul li').eq(0));
     $('.fig1 ul').css({ left : 0 });
